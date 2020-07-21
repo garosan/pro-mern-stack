57f1a070624b63df9532912661e794df85c9a9d2
Garo San <garosanchez27@gmail.com>
2020-07-20 23:50:20 -0500

    Add compile script

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
 const helloContinents = Array.from(continents, (c) => `Hello ${c}!`);
-const message = helloContinents.join(" ");
+const message = helloContinents.join("");
 
 const element = (
   <div title="Outer div">
